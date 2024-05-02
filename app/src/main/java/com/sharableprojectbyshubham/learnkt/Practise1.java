50e0ad469717f4afced182a038db17cebf9a9205
shubhamshreesri <websoftnv7@gmail.com>
2024-05-02 20:53:37 +0530

    52224somechanges

---
+++ app/src/main/java/com/sharableprojectbyshubham/learnkt/Practise1.java
@@ -0,0 +1,15 @@
+package com.sharableprojectbyshubham.learnkt;
+
+import java.util.Scanner;
+
+public class Practise1 {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        String str="shubham";
+        System.out.println(str);
+
+        Scanner scanner=new Scanner(System.in);
+        str=scanner.next();
+        System.out.print(str);
+    }
+}
---
+++ app/src/main/java/com/sharableprojectbyshubham/learnkt/practiseF1.kt
@@ -0,0 +1,36 @@
+package com.sharableprojectbyshubham.learnkt
+
+import java.util.logging.Logger.global
+
+fun main(){
+//    var str="shubham"
+//
+//
+//
+//    println(str[1])
+//    str="sri"
+//    println(str)
+//
+//    var name:String // If you want to create a String without assigning the value (and assign the value later), you must specify the type while declaring the variable:
+//    name = "John"
+//    println(name)
+//
+//    println("Enter a number")
+//    val a1= readLine()?.toInt()
+//    println(a1!!*a1!!)
+//
+//    var a=34;val b=34
+
+    val ite:Int=34
+    for(ite in 1..10)
+    {
+        println("5 * $ite = "+5*ite)
+    }
+
+    print(ite)
+
+}
+
+private infix fun Any.ite(println: Unit) {
+
+}
